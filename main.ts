ee8c8b22a36673faf438db0d1d21df94fe52610d
Daniel Escalante <d.escalante@uniandes.edu.co>
2023-04-05 11:30:44 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -3,9 +3,36 @@ import { series } from './data.js';
 
 let componente: HTMLElement = document.getElementById('series')!;
 let componentePromedio: HTMLElement = document.getElementById('promedio')!;
+let foto:HTMLElement = document.getElementById('imagen')!;
+let titulo:HTMLElement = document.getElementById('titulo')!;
+let descripcion: HTMLElement = document.getElementById('descripcion')!;
+let enlace: HTMLElement = document.getElementById('enlace')!;
+
+
 
 cargarFilas();
 componentePromedio.innerHTML = `Seasons average: ${calcularPromedio()}`;
+createBotones();
+
+
+
+function createBotones(){
+    series.forEach(c=>{
+        let boton = document.getElementById(`${c.id}`)!;
+        boton.onclick = () => {alterCard(boton.id)};
+    });
+}
+
+function alterCard(id: string){
+    let idR: number = parseInt(id);
+    let serie: Serie = series[idR - 1];
+    foto.setAttribute('src', serie.imagen);
+    titulo.innerHTML = `${serie.nombre}`;
+    descripcion.innerHTML = `${serie.descripcion}`;
+    enlace.setAttribute('href', `${serie.paginaWeb}`);
+    enlace.innerHTML = `${serie.paginaWeb}`;
+
+}
 
 
 function cargarFilas(): void{
@@ -18,8 +45,8 @@ function crearFila(serie: Serie):void{
     <td style = "font-weight: bold;"> 
         ${serie.id}
     </td>
-    <td style = "color:#547dde;">
-        ${serie.nombre}
+    <td style = "color:#547dde; hover">
+        <a id = "${serie.id}">${serie.nombre}</a>
     </td>
     <td>
         ${serie.cadena}
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,34 @@
 import { series } from './data.js';
 var componente = document.getElementById('series');
 var componentePromedio = document.getElementById('promedio');
+var foto = document.getElementById('imagen');
+var titulo = document.getElementById('titulo');
+var descripcion = document.getElementById('descripcion');
+var enlace = document.getElementById('enlace');
 cargarFilas();
 componentePromedio.innerHTML = "Seasons average: ".concat(calcularPromedio());
+createBotones();
+function createBotones() {
+    series.forEach(function (c) {
+        var boton = document.getElementById("".concat(c.id));
+        boton.onclick = function () { alterCard(boton.id); };
+    });
+}
+function alterCard(id) {
+    var idR = parseInt(id);
+    var serie = series[idR - 1];
+    foto.setAttribute('src', serie.imagen);
+    titulo.innerHTML = "".concat(serie.nombre);
+    descripcion.innerHTML = "".concat(serie.descripcion);
+    enlace.setAttribute('href', "".concat(serie.paginaWeb));
+    enlace.innerHTML = "".concat(serie.paginaWeb);
+}
 function cargarFilas() {
     series.forEach(function (c) { return crearFila(c); });
 }
 function crearFila(serie) {
     var fila = document.createElement('tr');
-    fila.innerHTML = ("\n    <td style = \"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style = \"color:#547dde;\">\n        ").concat(serie.nombre, "\n    </td>\n    <td>\n        ").concat(serie.cadena, "\n    </td>\n    <td>\n        ").concat(serie.temporadas, "\n    </td>"));
+    fila.innerHTML = ("\n    <td style = \"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style = \"color:#547dde; hover\">\n        <a id = \"").concat(serie.id, "\">").concat(serie.nombre, "</a>\n    </td>\n    <td>\n        ").concat(serie.cadena, "\n    </td>\n    <td>\n        ").concat(serie.temporadas, "\n    </td>"));
     componente.appendChild(fila);
 }
 function calcularPromedio() {
